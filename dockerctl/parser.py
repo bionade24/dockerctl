87c85b248ba5642217808839d0c835eeab241001
bionade24 <o.roesler@oscloud.info>
2019-02-23 00:56:10 +0100

    Small parser.py fix
    Signed-off-by: bionade24 <o.roesler@oscloud.info>

--- dockerctl/parser.py
+++ dockerctl/parser.py
@@ -8,17 +8,25 @@ version_nr = "0.1"
 
 
 def compose_start(option, compose_name):
-    for name in compose_name:
-        if option == "start":
-            executer.start(name)
-        elif option == "stop":
-            executer.stop(name)
-        elif option == "restart":
-            executer.restart(name)
-        elif option == "ps":
-            executer.processes(name)
-        else:
-            break
+    args = {
+        "start" : executer.start(compose_name),
+        "stop" : executer.stop(compose_name),
+        "restart" : executer.restart(compose_name),
+        "ps": executer.processes(compose_name),
+        "up" : executer.up(compose_name),
+        "kill" : executer.kill(compose_name),
+        "pull" : executer.pull(compose_name),
+        "push" : executer.push(compose_name),
+        "rm" : executer.remove(compose_name),
+        "top" : executer.top(compose_name),
+        "pause" : executer.pause(compose_name),
+        "unpause" : executer.unpause(compose_name),
+        "images" : executer.images(compose_name),
+        "port" : executer.port(compose_name),
+        "logs" : executer.logs(compose_name),
+    }
+    for i in option[1:]:
+        args[option](compose_name)
 
 
 def main(argv):
@@ -27,7 +35,9 @@ def main(argv):
 
     exgroup.add_argument('-v', '--version', action="version", version='dockerctl ' + version_nr,
                          help="print version number")
-    parser.add_argument('action', choices=['start', 'stop', 'restart', 'ps'])
+    parser.add_argument('--path', '--dir', type=str)
+    parser.add_argument('action', choices=['start', 'stop', 'restart', 'ps', 'up', ' kill', 'rm', 'top', 'logs',
+                                           'images', 'port', 'pull', 'push', 'pause', 'unpause'])
     parser.add_argument('compose_name', type=str, nargs='+')
 
     args = parser.parse_args(argv)
