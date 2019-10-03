0244e86cefcd3646c486f20d5bac8a713b562a78
bionade24 <o.roesler@oscloud.info>
2019-10-03 16:27:44 +0200

    Substituted command function dictionairy with getattr()

--- dockerctl/parser.py
+++ dockerctl/parser.py
@@ -2,32 +2,9 @@
 
 import sys
 import argparse
-from dockerctl import executer
+from dockerctl.executer import Commands
 
 version_nr = "0.2"
-
-
-def compose_start(option, compose_name):
-    args = {
-        "start"  : executer.start,
-        "stop"   : executer.stop,
-        "restart": executer.restart,
-        "ps"     : executer.processes,
-        "up"     : executer.up,
-        "kill"   : executer.kill,
-        "pull"   : executer.pull,
-        "push"   : executer.push,
-        "rm"     : executer.rm,
-        "top"    : executer.top,
-        "pause"  : executer.pause,
-        "unpause": executer.unpause,
-        "images" : executer.images,
-        "port"   : executer.port,
-        "logs"   : executer.logs,
-        "add"    : executer.add,
-        "remove" : executer.remove,
-    }
-    args[option](compose_name)
 
 
 def main(argv):
@@ -42,10 +19,7 @@ def main(argv):
     parser.add_argument('--path', metavar='PATH', type=str, help="Link path to yaml into /etc/docker/name")
 
     args = parser.parse_args(argv)
-
-    if args.path and args.command == "add":
-        executer.add(args.compose_name, args.path)
-    compose_start(args.command, args.compose_name)
+    getattr(Commands(args.compose_name, args.path), args.command)()
 
 
 if __name__ == '__main__':
