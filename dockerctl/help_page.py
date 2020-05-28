c05d70e8925da14c76fa45d73ab99b8ef9157fc2
bionade24 <o.roesler@oscloud.info>
2020-05-28 14:00:29 +0200

    Add help_page.py

---
+++ dockerctl/help_page.py
@@ -0,0 +1,30 @@
+
+
+def show():
+    explanation = '''
+usage: dockerctl [-h] [-v] [--path PATH] COMMAND COMPOSE_NAME [extra ARGS passed to docker-compose]
+             start: Start the composition
+             stop: Stop the composition
+             restart: Restart it.
+             ps: Show processes of services in composition.
+             up: Calls docker-compose up -d, composition runs as daemon afterwards.
+             kill: Kill the whole composition, if you don't pass extra args.
+             rm: Removes all volumes created by the compose yaml.
+             top: Get information about the processes in the services.
+             logs: Get the logs of the whole composition. Pass -f to get ongoing information.
+             images: Shows images used by the services in the composition.
+             port: Shows the port used and mapped by the composition.
+             pull: Pull images needed by the composition.
+             push: Push built images of the composition.
+             pause: Pause all services in the composition.
+             unpause: Unpause them.
+             add: Links the current dir to a folder with the compose_name under /etc/docker. 
+                Use --path to link an other path than curdir.
+             remove: Removes composition folder or link under /etc/docker
+             exec: Exec something in a service of a container. [ARGS] can optionally be used to write the command.
+             edit: Edit the docker-compose.yml. Uses the EDITOR env var.
+             show: Shows the docker-compose.yml in less.
+             create: Create dir with compos_name under /etc/docker
+             update: Runs pull and up in one command to update a composition.
+    '''
+    print(explanation)
